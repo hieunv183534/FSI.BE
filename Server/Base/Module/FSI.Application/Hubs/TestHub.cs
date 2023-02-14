50d828b8b0019466c61616e281bf1be57873b146
hieunv1 <hieunv1@ctin.vn>
2023-02-14 10:48:04 +0700

    uow on hub

--- Server/Base/Module/FSI.Application/Hubs/TestHub.cs
+++ Server/Base/Module/FSI.Application/Hubs/TestHub.cs
@@ -7,7 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
 using Volo.Abp.ObjectMapping;
+using Volo.Abp.Uow;
 
 namespace FSI.Application.Hubs
 {
@@ -16,20 +18,28 @@ namespace FSI.Application.Hubs
     {
         private readonly ITestRepository _testRepository;
         private readonly IObjectMapper _objectMapper;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
 
-        public TestHub(ITestRepository testRepository, IObjectMapper objectMapper)
+        public TestHub(ITestRepository testRepository, IObjectMapper objectMapper, IUnitOfWorkManager unitOfWorkManager)
         {
             _testRepository = testRepository;
             _objectMapper = objectMapper;
+            _unitOfWorkManager = unitOfWorkManager;
         }
 
         public async Task AddTest(CreateTestDto input)
         {
-            var rs = await _testRepository.InsertAsync(_objectMapper.Map<CreateTestDto, FSI.Domain.Test.Test>(input));
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var rs = await _testRepository.InsertAsync(_objectMapper.Map<CreateTestDto, FSI.Domain.Test.Test>(input));
+                await uow.CompleteAsync();
+                await Clients.All.SendAsync("OnCreatedTest", await _testRepository.GetListAsync());
+            }
+        }
 
-            await Clients.All.SendAsync("OnCreatedTest", await _testRepository.GetListAsync());
-
-            Task.CompletedTask.Wait();
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
