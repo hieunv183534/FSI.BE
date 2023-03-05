457aaf34cdd740ec90ca6ce04b25c4f5f27b6a2b
Hieu Nguyen <hieunv1@ctin.vn>
2023-03-05 22:13:53 +0700

    fix add to conversation

--- Server/Base/Module/FSI.Application/Hubs/ChatHub.cs
+++ Server/Base/Module/FSI.Application/Hubs/ChatHub.cs
@@ -4,6 +4,7 @@ using FSI.Domain.Test;
 using FSI.Domain.User;
 using Microsoft.AspNetCore.SignalR;
 using System.Security.Claims;
+using Volo.Abp.Domain.Repositories;
 using Volo.Abp.ObjectMapping;
 using Volo.Abp.Uow;
 
@@ -53,21 +54,37 @@ namespace FSI.Application.Hubs
         {
             using (var uow = _unitOfWorkManager.Begin(requiresNew: true, isTransactional: true))
             {
-                // khi một người add người khác vào conversation thì add vào db và ở dạng chưa active
-                await _userConversationRepository.InsertAsync(new UserConversation()
-                {
-                    ConversationId = conversationId,
-                    UserId = userId,
-                    EnableNotification = false,
-                    IsActive = false,
-                    RoleInConversation = Common.Enums.UserConversationRole.Member,
-                    LastIndexSeen= 0
-                });
+                var currentUserId = Guid.Parse(Context.GetHttpContext().User.FindFirst(ClaimTypes.NameIdentifier).Value);
 
-                // lấy connections của người đó để thông báo về việc add này.
-                // lấy hết các conncectionId tương ứng với các connection người đó ở các thiết bị khác nhau
-                var connectionIds = (await _userConnectionRepository.GetListAsync(uc => uc.UserId.Equals(userId))).Select(uc=> uc.ConnectionId);
-                await Clients.Clients(connectionIds.ToList()).SendAsync("BeAddedToAConversation", conversationId);
+                var userConversation = await _userConversationRepository.FirstOrDefaultAsync(uc => uc.UserId.Equals(currentUserId) && uc.ConversationId.Equals(conversationId));
+
+                if(userConversation == null)
+                {
+                    await Clients.Caller.SendAsync("ResultAddUserToConversation", 4031, "You not in this Conversation!");
+                }
+                else if(userConversation.RoleInConversation == Common.Enums.UserConversationRole.Owner ||
+                    userConversation.RoleInConversation == Common.Enums.UserConversationRole.Admin)
+                {
+                    // khi một người add người khác vào conversation thì add vào db và ở dạng chưa active
+                    await _userConversationRepository.InsertAsync(new UserConversation()
+                    {
+                        ConversationId = conversationId,
+                        UserId = userId,
+                        EnableNotification = false,
+                        IsActive = false,
+                        RoleInConversation = Common.Enums.UserConversationRole.Member,
+                        LastIndexSeen = 0
+                    });
+
+                    // lấy connections của người đó để thông báo về việc add này.
+                    // lấy hết các conncectionId tương ứng với các connection người đó ở các thiết bị khác nhau
+                    var connectionIds = (await _userConnectionRepository.GetListAsync(uc => uc.UserId.Equals(userId))).Select(uc => uc.ConnectionId);
+                    await Clients.Clients(connectionIds.ToList()).SendAsync("BeAddedToAConversation", conversationId);
+                }
+                else
+                {
+                    await Clients.Caller.SendAsync("ResultAddUserToConversation", 4032, "You are not admin of this Conversation");
+                }
             }
         }
 
