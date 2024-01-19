ac6e9e4745c08236c2b34e5aa03c04cc710ad6c9
hieunv1 <hieunv1@ctin.vn>
2024-01-19 16:41:54 +0700

    get image anonymous

---
+++ Server/Base/Module/FSI.Application/Blob/FileAppService.cs
@@ -0,0 +1,32 @@
+﻿using FSI.Application.Contracts.Blob;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.BlobStoring;
+
+namespace FSI.Application.Blob
+{
+    public class FileAppService : ApplicationService, IFileAppService
+    {
+
+        private readonly IBlobContainer _blobContainer;
+
+        public FileAppService(IBlobContainer blobContainer)
+        {
+            _blobContainer = blobContainer;
+        }
+
+        [AllowAnonymous]
+        [Route("image/{filePath}")]
+        public async Task<FileResult> GetImage([FromRoute]string filePath)
+        {
+            var blob = await _blobContainer.GetAllBytesOrNullAsync(filePath);
+            return new FileContentResult(blob, "image/png");
+        }
+    }
+}
