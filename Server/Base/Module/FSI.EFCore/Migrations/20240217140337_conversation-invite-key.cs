dbe1edc8146c55746e3b9408de9ea1d6679abcc8
Hieu Nguyen <hieunv1@ctin.vn>
2024-02-17 21:05:00 +0700

    migration

---
+++ Server/Base/Module/FSI.EFCore/Migrations/20240217140337_conversation-invite-key.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSI.EFCore.Migrations
+{
+    public partial class conversationinvitekey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MeetInviteKeys",
+                table: "Conversations",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeetInviteKeys",
+                table: "Conversations");
+        }
+    }
+}
